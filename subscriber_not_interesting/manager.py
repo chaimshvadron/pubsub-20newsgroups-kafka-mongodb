70302732bc46591daa2fca3825e49b44c6ea3977
ChaimShvadron <s0527649958@gmail.com>
2025-08-26 12:59:12 +0300

    Implement Kafka consumer with MongoDB integration and add message retrieval endpoint

---
+++ subscriber_not_interesting/manager.py
@@ -0,0 +1,21 @@
+from subscriber_interesting.mongo import MongoDBConnection
+from dal import MongoDAL
+    
+class MongoManager:
+    def __init__(self):
+        self.db = None
+        self.dal = None
+
+    def fetch_all(self, collection_name):
+        with MongoDBConnection() as conn:
+            db = conn.get_db()
+            dal = MongoDAL(db)
+            dal.ensure_collection(collection_name)
+            return dal.fetch_all(collection_name)
+
+    def insert_one(self, collection_name, document):
+        with MongoDBConnection() as conn:
+            db = conn.get_db()
+            dal = MongoDAL(db)
+            dal.ensure_collection(collection_name)
+            dal.insert_one(collection_name, document)
