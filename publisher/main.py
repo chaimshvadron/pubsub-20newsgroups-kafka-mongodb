503909f3b1910e5410c8feb4fe8a72657c39a8e6
ChaimShvadron <s0527649958@gmail.com>
2025-08-26 08:58:30 +0300

    Refactor message publishing logic for improved clarity and maintainability

--- publisher/main.py
+++ publisher/main.py
@@ -7,15 +7,15 @@ import random
 
 app = FastAPI()
 
-KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
+KAFKA_SERVERS = os.getenv("KAFKA_SERVERS", "localhost:9092")
 TOPICS = {
     "interesting": "interesting",
     "not_interesting": "not_interesting"
 }
 
 producer = KafkaProducer(
-    bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
-    
+    bootstrap_servers=KAFKA_SERVERS,
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
 def load_messages(file_path):
